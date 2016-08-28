3fc31022d7d920be337490798a5b6e92e6c01521
PetyoPlPetrov <petyoplpetrov@gmail.com>
2016-08-28 17:56:50 +0300

    init

---
+++ src/main/java/app/IssueTracker.java
@@ -0,0 +1,12 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTracker {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTracker.class, args);
+    }
+}
